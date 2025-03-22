39685d473882104b32e7cc43f9855937f2102a2f
Leslie Wylie <2780586913@qq.com>
2025-03-23 01:46:03 +0800

    更新context中架构的问题，改为了64位架构,这是2.0

--- kernel/process/context.c
+++ kernel/process/context.c
@@ -5,78 +5,61 @@
 void context_init(context_t *context, void (*entry)(void *), void *arg, void *stack_top)
 {
     memset(context, 0, sizeof(context_t));
-
-    // 设置入口点
     context->rip = (uint64_t)entry;
-
-    // 设置栈指针
     context->rsp = (uint64_t)stack_top;
-
-    // 设置参数 (x86_64 调用约定: 第一个参数在 rdi)
     context->rdi = (uint64_t)arg;
-
-    // 设置段寄存器
-    context->cs = 0x08; // 内核代码段
-    context->ds = 0x10; // 内核数据段
+    context->cs = 0x08;
+    context->ds = 0x10;
     context->es = 0x10;
     context->fs = 0x10;
     context->gs = 0x10;
     context->ss = 0x10;
-
-    // 设置标志寄存器（启用中断）
-    context->rflags = 0x202; // IF=1, IOPL=0
+    context->rflags = 0x202;
 }
 
 // 保存当前上下文
 void context_save(context_t *context)
 {
-    // 保存通用寄存器
-    __asm__ volatile(
-        "movq %%rax, %0\n"
-        "movq %%rbx, %1\n"
-        "movq %%rcx, %2\n"
-        "movq %%rdx, %3\n"
-        "movq %%rsi, %4\n"
-        "movq %%rdi, %5\n"
-        "movq %%rbp, %6\n"
-        "movq %%rsp, %7\n"
-        "movq %%r8,  %8\n"
-        "movq %%r9,  %9\n"
-        "movq %%r10, %10\n"
-        "movq %%r11, %11\n"
-        "movq %%r12, %12\n"
-        "movq %%r13, %13\n"
-        "movq %%r14, %14\n"
-        "movq %%r15, %15\n"
-        : "=m"(context->rax), "=m"(context->rbx),
-          "=m"(context->rcx), "=m"(context->rdx),
-          "=m"(context->rsi), "=m"(context->rdi),
-          "=m"(context->rbp), "=m"(context->rsp),
-          "=m"(context->r8), "=m"(context->r9),
-          "=m"(context->r10), "=m"(context->r11),
-          "=m"(context->r12), "=m"(context->r13),
-          "=m"(context->r14), "=m"(context->r15)
+    __asm__ __volatile__(
+        "movq %%rax, 0(%0)\n\t"
+        "movq %%rbx, 8(%0)\n\t"
+        "movq %%rcx, 16(%0)\n\t"
+        "movq %%rdx, 24(%0)\n\t"
+        "movq %%rsi, 32(%0)\n\t"
+        "movq %%rdi, 40(%0)\n\t"
+        "movq %%rbp, 48(%0)\n\t"
+        "movq %%rsp, 56(%0)\n\t"
+        "movq %%r8,  64(%0)\n\t"
+        "movq %%r9,  72(%0)\n\t"
+        "movq %%r10, 80(%0)\n\t"
+        "movq %%r11, 88(%0)\n\t"
+        "movq %%r12, 96(%0)\n\t"
+        "movq %%r13, 104(%0)\n\t"
+        "movq %%r14, 112(%0)\n\t"
+        "movq %%r15, 120(%0)\n\t"
         :
+        : "r"(context)
         : "memory");
 
     // 保存段寄存器
-    __asm__ volatile(
-        "movw %%cs, %0\n"
-        "movw %%ds, %1\n"
-        "movw %%es, %2\n"
-        "movw %%fs, %3\n"
-        "movw %%gs, %4\n"
-        "movw %%ss, %5\n"
-        : "=m"(context->cs), "=m"(context->ds),
-          "=m"(context->es), "=m"(context->fs),
-          "=m"(context->gs), "=m"(context->ss)
-        :
-        : "memory");
+    __asm__ __volatile__(
+        "movw %%cs, %0\n\t"
+        "movw %%ds, %1\n\t"
+        "movw %%es, %2\n\t"
+        "movw %%fs, %3\n\t"
+        "movw %%gs, %4\n\t"
+        "movw %%ss, %5\n\t"
+        : "=m"(context->cs),
+          "=m"(context->ds),
+          "=m"(context->es),
+          "=m"(context->fs),
+          "=m"(context->gs),
+          "=m"(context->ss));
 
     // 保存标志寄存器
-    __asm__ volatile(
-        "pushfq\n"
-        "popq %0\n"
+    __asm__ __volatile__(
+        "pushfq\n\t"
+        "popq %0\n\t"
         : "=m"(context->rflags)
         :
         : "memory");
@@ -86,60 +69,63 @@ void context_save(context_t *context)
 void context_restore(context_t *context)
 {
     // 恢复段寄存器
-    __asm__ volatile(
-        "movw %0, %%ds\n"
-        "movw %0, %%es\n"
-        "movw %0, %%fs\n"
-        "movw %0, %%gs\n"
+    __asm__ __volatile__(
+        "movw %0, %%ds\n\t"
+        "movw %0, %%es\n\t"
+        "movw %0, %%fs\n\t"
+        "movw %0, %%gs\n\t"
         :
-        : "r"(context->ds)
+        : "r"((uint16_t)context->ds)
         : "memory");
 
     // 恢复通用寄存器
-    __asm__ volatile(
-        "movq %0, %%rax\n"
-        "movq %1, %%rbx\n"
-        "movq %2, %%rcx\n"
-        "movq %3, %%rdx\n"
-        "movq %4, %%rsi\n"
-        "movq %5, %%rdi\n"
-        "movq %6, %%rbp\n"
-        "movq %7, %%rsp\n"
-        "movq %8, %%r8\n"
-        "movq %9, %%r9\n"
-        "movq %10, %%r10\n"
-        "movq %11, %%r11\n"
-        "movq %12, %%r12\n"
-        "movq %13, %%r13\n"
-        "movq %14, %%r14\n"
-        "movq %15, %%r15\n"
+    __asm__ __volatile__(
+        "movq 0(%0), %%rax\n\t"
+        "movq 8(%0), %%rbx\n\t"
+        "movq 16(%0), %%rcx\n\t"
+        "movq 24(%0), %%rdx\n\t"
+        "movq 32(%0), %%rsi\n\t"
+        "movq 40(%0), %%rdi\n\t"
+        "movq 48(%0), %%rbp\n\t"
+        "movq 56(%0), %%rsp\n\t"
+        "movq 64(%0), %%r8\n\t"
+        "movq 72(%0), %%r9\n\t"
+        "movq 80(%0), %%r10\n\t"
+        "movq 88(%0), %%r11\n\t"
+        "movq 96(%0), %%r12\n\t"
+        "movq 104(%0), %%r13\n\t"
+        "movq 112(%0), %%r14\n\t"
+        "movq 120(%0), %%r15\n\t"
         :
-        : "m"(context->rax), "m"(context->rbx),
-          "m"(context->rcx), "m"(context->rdx),
-          "m"(context->rsi), "m"(context->rdi),
-          "m"(context->rbp), "m"(context->rsp),
-          "m"(context->r8), "m"(context->r9),
-          "m"(context->r10), "m"(context->r11),
-          "m"(context->r12), "m"(context->r13),
-          "m"(context->r14), "m"(context->r15)
-        : "memory");
+        : "r"(context)
+        : "memory", "rax", "rbx", "rcx", "rdx",
+          "rsi", "rdi", "rbp", "rsp",
+          "r8", "r9", "r10", "r11",
+          "r12", "r13", "r14", "r15");
 
     // 恢复标志寄存器并跳转到rip
-    __asm__ volatile(
-        "pushq %0\n" // rflags
-        "popfq\n"
-        "pushq %1\n" // cs
-        "pushq %2\n" // rip
-        "iretq\n"
+    __asm__ __volatile__(
+        "pushq %0\n\t" // ss
+        "pushq %1\n\t" // rsp
+        "pushq %2\n\t" // rflags
+        "pushq %3\n\t" // cs
+        "pushq %4\n\t" // rip
+        "iretq\n\t"
         :
-        : "r"(context->rflags),
+        : "r"((uint64_t)context->ss),
+          "r"(context->rsp),
+          "r"(context->rflags),
           "r"((uint64_t)context->cs),
-          "r"(context->rip));
+          "r"(context->rip)
+        : "memory");
 }
 
 // 切换上下文
 void context_switch(context_t *from, context_t *to)
 {
-    context_save(from);
-    context_restore(to);
+    if (from && to)
+    {
+        context_save(from);
+        context_restore(to);
+    }
 }
